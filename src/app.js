aa4f6916d109b840b15986ca6ed0dce152a76e70
Boyan Iliev <97385003+Boyan253@users.noreply.github.com>
2022-11-01 21:21:14 +0200

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,43 @@
+
+
+// showView('#home-page')
+
+import { createPage } from "./create.js";
+import { homePage } from "./home.js";
+import { loginPage } from "./login.js";
+import { registerPage } from "./register.js";
+import { updateNav } from "./util.js";
+
+
+const routes = {
+  '/': homePage,
+  '/login': loginPage,
+  '/logout': logout,
+  '/register': registerPage,
+  '/create': createPage
+ 
+}
+
+document.querySelector('nav').addEventListener('click', onNavigate)
+document.querySelector('#add-movie-button a').addEventListener('click', onNavigate)
+
+function onNavigate(event){
+  if(event.target.tagName == 'A' && event.target.href){
+    event.preventDefault()
+    const url = new URL(event.target.href)
+
+    const view = routes[url.pathname]
+    if(typeof view == 'function'){
+      view()
+    }
+    
+    
+  }
+}
+
+function logout(){
+localStorage.removeItem('user')
+updateNav()
+}
+updateNav()
+homePage()
---
+++ src/edit.js
@@ -0,0 +1,8 @@
+import { showView } from "./util.js"
+
+const section = document.querySelector('#edit-movie')
+
+
+function editPage(){
+  showView(section)
+}
